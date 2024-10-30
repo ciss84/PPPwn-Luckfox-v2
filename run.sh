63fca173a583877993e8fd95848ecf2497f3a950
Mugiwara <37186888+ciss84@users.noreply.github.com>
2024-10-30 17:48:00 +0100

    Add files via upload

--- run.sh
+++ run.sh
@@ -22,6 +22,7 @@ HALT_CHOICE=$(read_json 'HALT_CHOICE')
 PPPWN_EXEC=$(read_json 'PPPWN_EXEC')
 DIR=$(read_json 'install_dir')
 LOG_FILE=$(read_json 'log_file')
+EN_NET=$(read_json 'en_inet')
 RESTMODE=$(read_json 'RESTMODE');
 PPPOE_WAIT=$(read_json 'PPPOE_WAIT');
 
@@ -38,6 +39,25 @@ CMD="$DIR/$PPPWN_EXEC --interface eth0 --fw $FW_VERSION --stage1 $STAGE1_FILE --
 [ "$AUTO_RETRY" == "true" ] && CMD="$CMD --auto-retry"
 [ "$NO_WAIT_PADI" == "true" ] && CMD="$CMD --no-wait-padi"
 [ "$REAL_SLEEP" == "true" ] && CMD="$CMD --real-sleep"
+
+start_internet() {
+  echo "Bringing up wlan0..."
+  ifconfig wlan0 up
+  wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant.conf
+  udhcpc -i wlan0
+  echo "Setting up bridge between eth0 and wlan0..."
+  brctl addbr br0
+  brctl addif br0 eth0
+  brctl addif br0 wlan0
+  ifconfig br0 up
+  udhcpc -i br0
+}
+kill_net() {
+  ifconfig eth0 down
+  ifconfig wlan0 down
+  ifconfig br0 down
+  brctl delbr br0
+}
 
 ifdown() {
     printf "Shutting down interface eth0... "
@@ -142,7 +162,13 @@ check_status() {
 start_services() {
   ifup
 	# Start PPPoE server, nginx, php-fpm
-  pppoe-server -I eth0 -T 60 -N 1 -C isp -S isp -L 192.168.1.1 -R 192.168.1.2 &
+  if [ "$EN_NET" == "true" ]; then
+        pppoe-server -I br0 -T 60 -N 10 -C isp -S isp -L 0.0.0.0 -R 0.0.0.0 &
+        sleep 5
+        start_internet
+  else
+        pppoe-server -I eth0 -T 60 -N 1 -C isp -S isp -L 192.168.1.1 -R 192.168.1.2 &
+  fi  
   /etc/init.d/S50nginx start
   /etc/init.d/S49php-fpm start
 }
